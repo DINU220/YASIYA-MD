5080ee0761511f04d75aedf3b77e3ea9f578ed52
gojo-SL <Ddineth009@gmail.com>
2025-07-28 21:29:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~dRRigAZb#kD78w7RiLeIo_98OxzJ89klUZUrVt9ji8jiJRXWuJxw'
 };
